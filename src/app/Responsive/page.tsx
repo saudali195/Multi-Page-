afdbb830c17fbd83d747af416bf04d2087c9d0bb
saudali195 <160694354+saudali195@users.noreply.github.com>
2024-11-10 23:51:21 +0500

    Add files via upload

---
+++ src/app/Responsive/page.tsx
@@ -0,0 +1,10 @@
+import Styles from "./page.module.css"
+export default function Responsive(){
+    return(
+        <div className={Styles.container}>
+            <div className={Styles.testingResponsive}>
+
+            </div>
+        </div>
+    )
+}
