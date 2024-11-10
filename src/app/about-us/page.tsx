48ffc9821ef475b880c51b85342b95aafaa5ee5d
saudali195 <160694354+saudali195@users.noreply.github.com>
2024-11-10 23:44:45 +0500

    Add files via upload

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,18 @@
+
+    import React from "react"
+    export default function About(){
+        return(
+            <div className="parentContainer">
+            <div className="childContainer1">
+            <div className="title">About Me</div>
+            <div className="imageContainer"></div>
+            <div className="description">
+                <p className="text">My name is Saad. I am a Front-end Developer.</p>
+    
+
+            <p className="text1">  As a frontend developer, I specialize in crafting responsive, user-friendly, and visually engaging interfaces. With a strong foundation in HTML, CSS, and JavaScript, I am skilled in translating design concepts into functional, interactive web experiences. My focus is on writing clean, efficient code that enhances both performance and user satisfaction, and I continuously strive to stay updated with the latest technologies to create seamless and accessible applications.</p>
+            </div>
+            </div>
+            </div>
+        )
+    }
