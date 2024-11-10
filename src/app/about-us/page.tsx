23f85b25e19035bcb888c04cd4d5a792c7527dd9
saudali195 <160694354+saudali195@users.noreply.github.com>
2024-11-11 00:10:37 +0500

    page.tsx

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -7,7 +7,7 @@
             <div className="title">About Me</div>
             <div className="imageContainer"></div>
             <div className="description">
-                <p className="text">My name is Saud. I am a Front-end Developer.</p>
+                <p className="text">My name is Saud Muhammad Ali AKA Xyron I am a future gen AI cloud Developer.</p>
     
 
             <p className="text1">  As a frontend developer, I specialize in crafting responsive, user-friendly, and visually engaging interfaces. With a strong foundation in HTML, CSS, and JavaScript, I am skilled in translating design concepts into functional, interactive web experiences. My focus is on writing clean, efficient code that enhances both performance and user satisfaction, and I continuously strive to stay updated with the latest technologies to create seamless and accessible applications.</p>
